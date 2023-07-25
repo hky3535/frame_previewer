b9ea84d3244bebdfa0796e145f1f8fd6afb487c7
hky3535 <hky3535@163.com>
2023-07-25 08:58:03 +0000

    update onekey apply

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ class Main:
         # 启动HTTP服务器
         http_server_thread = threading.Thread(
             target=self.basic.index_html, 
-            args=("0.0.0.0", 50001)
+            args=("0.0.0.0", 30000)
         )
         http_server_thread.start()
 
         # 启动ws服务器
         ws_server_thread = threading.Thread(
             target=self.basic.index_ws, 
-            args=("0.0.0.0", 50002)
+            args=("0.0.0.0", 30001)
         )
         ws_server_thread.start()
+
+        while True:
+            time.sleep(1)
 
 
 if __name__ == "__main__":
